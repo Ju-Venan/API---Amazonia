439f913ca6f191daddb1627582c2003a454f05af
Julianne Venancio <venanciojulianne@gmail.com>
2025-05-01 19:57:04 -0300

    primeiro commit

---
+++ src/routes/amazonia.routes.js
@@ -0,0 +1,7 @@
+// src/routes/amazonRoutes.js
+import express from 'express';
+import AmazonController from "../controller/amazonia.controller.js"
+
+const router = express.Router();
+
+export default router;
