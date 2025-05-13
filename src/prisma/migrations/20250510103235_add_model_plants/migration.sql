0c7b0b3327f18b6d64510a18f31b72f146b3937f
tereza <brunaaasilvaaa261@gmail.com>
2025-05-13 09:58:35 -0300

    :sparkles: feat: Adicionando modelo Plantas e migrações

---
+++ src/prisma/migrations/20250510103235_add_model_plants/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Plant" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "specie" TEXT NOT NULL,
+    "botanicalDescription" TEXT NOT NULL,
+    "naturalHabitat" TEXT NOT NULL,
+    "benefits" TEXT NOT NULL
+);
