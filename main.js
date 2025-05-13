16d52e189bb62ec8448d0456c1cdffbcd359af2e
tereza <brunaaasilvaaa261@gmail.com>
2025-05-13 10:20:47 -0300

    :sparkles: feat: Funcionalidades que garantem funcionamento do Prisma, Controller, Services e Routes

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import router from "./src/routes/amazonia.routes.js";
+import plantsRouter from './src/routes/plants.routes.js';
+import dotenv from "dotenv"
 
+// lendo o arquivo .env
+dotenv.config();
+
+// iniciando a biblioteca express
 const app = express();
-const PORT = 3000;
+
+// informando que a porta será a passada no arquivo .env ou 3000
+const PORT = process.env.BACKEND_PORT || 3000;
 
 app.use(express.json());
+
+//Rotas
 app.use(router); 
+app.use("/plantas", plantsRouter );
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
