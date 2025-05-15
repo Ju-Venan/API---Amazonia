5ec633763598fdc84ade51d174f9b1d7933827d2
Nataly-Costaa <natalynaty653@gmail.com>
2025-05-15 20:38:41 -0300

    :sparkles: feat: add Prisma ORM and database schema

---
+++ prisma/migrations/20250515232921_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Threat" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
