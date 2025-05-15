b3d4df5e67c416129cb0639769fb39c2a05cf965
Nataly-Costaa <natalynaty653@gmail.com>
2025-05-15 20:59:08 -0300

    :sparkles: feat: adding the threat route

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import router from "./src/routes/amazonia.routes.js";
+import threat from "./src/routes/threat.routes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(router); 
+
+app.use("/threat", threat);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
