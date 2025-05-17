69742bf22de72062bd247aae855e0cba33b90da1
Nataly-Costaa <natalynaty653@gmail.com>
2025-05-17 18:26:05 -0300

    test_tube: test: add tests for Threat routes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import express from 'express';
 // import router from "./src/routes/amazonia.routes.js";
-import threat from "./src/routes/threat.routes.js";
+import threatRouter from "./src/routes/threat.routes.js";
 import { logEvents, logger } from "./src/middleware/logger.middleware.js";
 import dotenv from "dotenv";
 
@@ -13,7 +13,7 @@ app.use(logger);
 app.use(express.json());
 
 // app.use(router); 
-app.use("/threat", threat);
+app.use("/threat", threatRouter);
 
 app.listen(PORT, () => {
     logEvents(`Servidor rodando na porta ${PORT}`, "listen.log");
