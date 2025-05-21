f881caeb4289ebbdb4a9deeec21c728770a516b6
tereza <brunaaasilvaaa261@gmail.com>
2025-05-20 21:43:54 -0300

    :sparkles: feat: Adicionando o CORS

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import { swaggerSpec, swaggerUi } from './swagger.js';
 // import router from "./src/routes/amazonia.routes.js";
 import plantsRouter from './src/routes/plants.routes.js';
+import corsMiddleware from './src/middlewares/cors.middleware.js'
 
 // iniciando a biblioteca express
 const app = express();
 
 app.use(express.json());
+app.use(corsMiddleware);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 //Rotas
 // app.use(router); 
