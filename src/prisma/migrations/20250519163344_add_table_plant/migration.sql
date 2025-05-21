3ef529b724e99e47d024f30f79727a3902125ae1
tereza <brunaaasilvaaa261@gmail.com>
2025-05-20 21:41:31 -0300

    wrench: chore: muda as configurações de banco de dados para PostgreSQL

--- src/prisma/migrations/20250519163344_add_table_plant/migration.sql
+++ src/prisma/migrations/20250519163344_add_table_plant/migration.sql
@@ -1,9 +1,11 @@
 -- CreateTable
 CREATE TABLE "Plant" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "specie" TEXT NOT NULL,
     "botanicalDescription" TEXT NOT NULL,
     "naturalHabitat" TEXT NOT NULL,
-    "benefits" TEXT NOT NULL
+    "benefits" TEXT NOT NULL,
+
+    CONSTRAINT "Plant_pkey" PRIMARY KEY ("id")
 );
