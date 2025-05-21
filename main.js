a2907371d9637665ea4238533878b1d8e21a1632
samuel <marquessam02@gmail.com>
2025-05-21 20:45:25 -0300

    refactor: ajustando a chamada das rotas

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
-import express from 'express';
-import curiosidadesRoutes from './src/routes/curiosidades.routes.js';
-import amazoniaRoutes from './src/routes/amazonia.routes.js';
+
+
 import dotenv from "dotenv";
 dotenv.config();
 
 import app from './server.js';
 
-app.use(express.json());
-app.use('/api', curiosidadesRoutes);
-app.use('/api', amazoniaRoutes);
-
 const PORT = 3000;
-app.listen(() => { // RODA A PORTA 3000 NO SERVIDOR
+app.listen(PORT,() => { // RODA A PORTA 3000 NO SERVIDOR
     console.log(`Servidor rodando em http://localhost:${PORT}`,);
 });
 
--- server.js
+++ server.js
@@ -4,19 +4,19 @@ import animais from "./src/routes/animal.Route.js";
 import plantsRouter from "./src/routes/plants.routes.js"
 import threatRouter from "./src/routes/threat.routes.js";
 import corsMiddleware from './src/middlewares/cors.js';
-
+import curiosidadesRouter from './src/routes/curiosidades.routes.js';
 const app = express();
 
 app.use(express.json());
 
 // Aplicar middlewares
 app.use(corsMiddleware); // Aplicar o middleware do CORS antes de outros middlewares
-
+app.use(animais);
 // Usar as rotas
 app.use("/users", userRouter);
 app.use('/animal', animais);
 app.use("/plantas", plantsRouter);
 app.use("/threat", threatRouter);
-
+app.use("/curiosidades", curiosidadesRouter);
 export default app; //exporta o app para usar o express
 
