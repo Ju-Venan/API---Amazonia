bd171a47a99733b60781a12cd5b931d2ea2961bc
Nataly-Costaa <natalynaty653@gmail.com>
2025-05-22 14:57:02 -0300

    :test_tube: test: Criando teste para curiosidades

--- src/routes/curiosidades.routes.js
+++ src/routes/curiosidades.routes.js
@@ -3,10 +3,10 @@ import CuriosidadesController from '../controller/curiosidades.controller.js';
 
 const router = express.Router();
 
-router.get('/curiosidades', CuriosidadesController.getCuriosidades);
-router.get('/curiosidades/:id', CuriosidadesController.getById);
-router.post('/curiosidades/criar', CuriosidadesController.create);
-router.put('/curiosidades/atualizar/:id', CuriosidadesController.update);
-router.delete('/curiosidades/deletar/:id', CuriosidadesController.delete);
+router.get('/', CuriosidadesController.getCuriosidades);
+router.get('/:id', CuriosidadesController.getById);
+router.post('/criar', CuriosidadesController.create);
+router.put('/atualizar/:id', CuriosidadesController.update);
+router.delete('/deletar/:id', CuriosidadesController.delete);
 
 export default router;
